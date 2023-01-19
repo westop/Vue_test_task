a753312159b54d4e9c0dfd9ccd853c1c8b96768d
westop <85644177+westop@users.noreply.github.com>
2023-01-19 07:28:31 +0200

    изменено:      src/pages/MapPage.vue

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,26 +1,35 @@
 <template>
     <div>
-        <div id="map"></div>
+        <div id="map" @click="onMapClick"></div>
     </div>
 </template>
 
 <script>
-import leaflet from "leaflet";
+import L from "leaflet";
 import { onMounted, ref } from "vue";
 import axios from "axios";
 export default {
     name: "Home",
     setup() {
-        let map;
+        
         onMounted(() => {
-            map = leaflet.map('map').setView([48, 31], 6);
-            leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            map = L.map('map').setView([48, 31], 6);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(map);
+           
         })
 
     },
+    methods:{
+        onMapClick(){
+            map.on('click', function(e) {
+                var marker = new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map)
+                console.log(e.latlng.lat, e.latlng.lng)
+            });
+        } 
+    }
 
 }
 </script>
@@ -28,9 +37,9 @@ export default {
 <style scoped>
 #map {
     position: fixed;
-  top: 50px;
-  left: 0;
-  width: 100%;
-  height: 100%;
+    top: 50px;
+    left: 0;
+    width: 100%;
+    height: 100%;
 }
 </style>
