0e7fff2b777ac3025e5a814e720098bda7b09f82
westop <85644177+westop@users.noreply.github.com>
2023-01-21 10:28:16 +0200

    переименовано: dist/css/app.c51c577e.css -> dist/css/app.7a1c58ca.css 	изменено:      dist/index.html 	удалено:       dist/js/app.4c00cb6b.js 	удалено:       dist/js/app.4c00cb6b.js.map 	новый файл:    dist/js/app.502b5a20.js 	новый файл:    dist/js/app.502b5a20.js.map 	изменено:      src/pages/MapPage.vue

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,12 +1,14 @@
 <template>
-    <div> 
-        
-        <div id="map" @click="onMapClick"></div>
+    <div>
+
+        <div id="map"></div>
         <my-dialog v-model:show="dialogVisible">
-           
+            <my-input></my-input>
+            <my-input></my-input>
+            <my-input></my-input>
         </my-dialog>
-       
-        
+
+
     </div>
 </template>
 
@@ -17,45 +19,76 @@ import axios from "axios";
 
 
 import PostForm from "@/components/PostForm";
-
+import MyInput from "@/components/UI/MyInput";
 export default {
     name: "App",
-    
+
     components: {
         PostForm,
+        MyInput,
     },
     data() {
-        return{
-            dialogVisible:true,
-            
+        return {
+            dialogVisible: false,
+            markers: [{
+                title: "1",
+                lat: 49.62318710125795,
+                lng: 31.68655843732102
+            },
+            {
+                title: "2",
+                lat: 48.4121615523613,
+                lng: 31.73051384182521
+            },
+            {
+                title: "3",
+                lat: 48.746682702488485,
+                lng: 36.7194522530482
+            },
+
+            ]
+
         }
     },
+    methods: {
+        loadMarkers() {
+            for (const item of this.markers) {
+                L.marker([item.lat, item.lng])
+                    .bindPopup(item.title).openPopup().addTo(map)
 
+            }
+            console.log("ok")
+        },
+        showDialog() {
+            this.dialogVisible = true;
+        },
+        addMarker() {
 
+            map.on('click', function (e) { 
+                //this.dialogvisible = true;
+                L.marker([e.latlng.lat, e.latlng.lng])
+                    .bindPopup("qwefgqwf").openPopup().addTo(map)
+                console.log(e.latlng);
+                //this.dialogvisible = false;
+            });
 
-    setup() {
+            
+        },
         
-        onMounted(() => {
-            map = L.map('map').setView([48, 31], 6);
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
-           
-        })
+    },
+    mounted() {
+        map = L.map('map').setView([48, 31], 6);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        this.loadMarkers();
+        this.addMarker();
+    },
+    watch: {
+
 
     },
-    methods:{
-        onMapClick(){
-            map.on('click', function(e) {
-                let marker = new L.marker([e.latlng.lat, e.latlng.lng])
-                .bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup().addTo(map)
-                console.log("ok")
-                
-            });
-        } ,
-    }
-
 }
 </script>
 
